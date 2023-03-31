13a20226471b0e52f569555d16a8b3d311132f23
Nicolasvela <nikovelandia01@gmail.com>
2023-03-31 18:51:38 -0500

    juego
    juego

--- BatallaNaval/Program.cs
+++ BatallaNaval/Program.cs
@@ -15,12 +15,30 @@ string boatlength = Console.ReadLine();
 
 Console.WriteLine("Ingrese las coordenadas del barco de la forma ===> x,y");
 
+int n = 10;
+
+    if (n > 10 || n <= 0)
+    {
+        n = 10;
+    }
+    int a = n;
+    int b = n;
+    int[,] arrayTablero = new int[a, b];
+
+    for (int i = 0; i < arrayTablero.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayTablero.GetLength(1); j++)
+        {
+            Console.Write(" ~ ");
+        }
+        Console.Write("\n");
+    }
 
 
 
 
 
 
-string boatstart = Console.ReadLine();
+    string boatstart = Console.ReadLine();
 string[] coordinates = boatstart.Split(',');    
 
