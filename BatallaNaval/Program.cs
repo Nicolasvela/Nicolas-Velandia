806c417823921c58a87c96347a429e97b9147879
Nicolasvela <nikovelandia01@gmail.com>
2023-03-31 18:28:58 -0500

    batallanaval
    batallanaval

--- BatallaNaval/Program.cs
+++ BatallaNaval/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("BATALLA NAVAL");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Jugador 1");
+Console.WriteLine("Seleccione la forma en la que desea ubicar el barco:   0=vertical  1=horizontal");
+string boatposition = Console.ReadLine();
+
+
+Console.WriteLine("Ingrese la longitud del barco (Tablero de 5x5)");
+string boatlength = Console.ReadLine();
+
+
+Console.WriteLine("Ingrese las coordenadas del barco de la forma ===> x,y");
+
+
+
+
+
+
+
+string boatstart = Console.ReadLine();
+string[] coordinates = boatstart.Split(',');    
+
