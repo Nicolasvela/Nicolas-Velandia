53c087d8f94b9d661496566b979f21824e1e9ce9
Nicolasvela <nikovelandia01@gmail.com>
2023-04-01 15:24:41 -0500

    batallanaval
    batallanaval

--- BatallaNaval/Program.cs
+++ BatallaNaval/Program.cs
@@ -35,13 +35,43 @@ int n = 10;
             Console.Write(sea);
         }
         Console.Write("\n");
-    }
-
-
-
-
-
+}
 
     string boatstart = Console.ReadLine();
-string[] coordinates = boatstart.Split(',');    
+string[] coordinates = boatstart.Split(',');
 
+
+
+Console.WriteLine($"x= {coordinates[0]}");
+Console.WriteLine($"y= {coordinates[1]}");
+
+Console.WriteLine("Ingrese cualquier tecla para continuar");
+Console.ReadLine();
+Console.Clear();
+
+
+string fail = "X";
+string goal = "0";
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("JUGADOR 2");
+Console.WriteLine("Ataca al barco enemigo, ingrese la ubicación de la forma ===> x,y ");
+
+if (n > 10 || n <= 0)
+{
+    n = 10;
+}
+
+for (int i = 0; i < arrayTablero.GetLength(0); i++)
+{
+    for (int j = 0; j < arrayTablero.GetLength(1); j++)
+    {
+        Console.Write(sea);
+    }
+    Console.Write("\n");
+}
+string boatattack = Console.ReadLine();
+string[] coordinatesAttack = boatattack.Split(',');
+string x = coordinatesAttack[0];
+string y = coordinatesAttack[1];
